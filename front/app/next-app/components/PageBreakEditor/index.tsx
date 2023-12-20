c3d8e7c079b3626128d4d96041333db3085122c2
ICHINOMIYA <ichiryo108@gmail.com>
2023-12-20 15:47:02 +0900

    高さを取得するコードの追加

--- front/app/next-app/components/PageBreakEditor/index.tsx
+++ front/app/next-app/components/PageBreakEditor/index.tsx
@@ -3,7 +3,7 @@ import StarterKit from "@tiptap/starter-kit";
 import Image from "@tiptap/extension-image";
 import Highlight from "@tiptap/extension-highlight";
 import TextAlign from "@tiptap/extension-text-align";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Node } from "@tiptap/core";
 
 const PageNode = Node.create({
@@ -39,40 +39,6 @@ const PageNode = Node.create({
   },
 });
 
-const MAX_HEIGHT = 300; // ページの最大高さ
-
-const createPaginatedDOM = (htmlDOM: string) => {
-  const container = document.createElement("div");
-  container.innerHTML = htmlDOM;
-
-  let currentPage = document.createElement("div");
-  currentPage.className = "page";
-
-  const pages = [];
-  let currentHeight = 0;
-
-  Array.from(container.childNodes).forEach((node) => {
-    const clone = node.cloneNode(true) as HTMLElement;
-    currentPage.appendChild(clone);
-    currentHeight += clone.offsetHeight;
-
-    if (currentHeight >= MAX_HEIGHT) {
-      pages.push(currentPage.outerHTML);
-      currentPage = document.createElement("div");
-      currentPage.className = "page";
-      currentHeight = 0;
-    }
-  });
-
-  if (currentPage.childNodes.length > 0) {
-    pages.push(currentPage.outerHTML);
-  }
-
-  console.log(pages.join(""));
-
-  return pages.join("");
-};
-
 export default function Home({ setData, data, setContent }: any) {
   const editor = useEditor({
     extensions: [
@@ -90,8 +56,13 @@ export default function Home({ setData, data, setContent }: any) {
     onUpdate: ({ editor }) => {
       // 文字が入力されるたびに実行される
       const html = editor.getHTML(); // 現在のHTMLを取得
-      console.log(html); // コンソールにHTMLを表示
-      createPaginatedDOM(html);
+      const children =
+        editorContainerRef.current.querySelector(".tiptap").children;
+      let heightSum = 0;
+      Array.from(children).forEach((child) => {
+        heightSum += child.offsetHeight;
+      });
+      console.log(heightSum);
     },
     content: content,
     onBlur: ({ editor }: any) => {
@@ -101,8 +72,19 @@ export default function Home({ setData, data, setContent }: any) {
     },
   });
 
+  const editorContainerRef = useRef(null);
+
+  const [totalHeight, setTotalHeight] = useState(0);
+
+  // エディタのインスタンスを使用する例
+  const logContent = () => {
+    if (editorContainerRef.current) {
+      console.log(editorContainerRef.current.getHTML());
+    }
+  };
+
   return (
-    <div>
+    <div ref={editorContainerRef}>
       <EditorContent
         editor={editor}
         className="p-4 min-w-xl min-h-xl mx-auto"
