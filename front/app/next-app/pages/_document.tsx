4cb63e17c9eb6b78b1a97fb24c46c8452d8ad4ee
ichinomiya1038 <ichiryo108@gmail.com>
2024-01-09 14:41:50 +0900

    Vercel Analyze

--- front/app/next-app/pages/_document.tsx
+++ front/app/next-app/pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Analytics />
         <Main />
         <NextScript />
       </body>
