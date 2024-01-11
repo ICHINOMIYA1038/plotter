37710f354bfcfabee2077967896054c4e92bac9b
= <=>
2024-01-11 09:41:28 +0900

    ファイル名がnullになるのを修正

--- front/app/next-app/components/EditorToJson.ts
+++ front/app/next-app/components/EditorToJson.ts
@@ -109,7 +109,7 @@ export const EditorToTextFile = (editor: Editor) => {
   const link = document.createElement("a");
   link.href = URL.createObjectURL(blob);
 
-  const savedName = localStorage.getItem("projectName");
+  const savedName = localStorage.getItem("projectName") || "無題のプロジェクト";
   link.download = `${savedName}.txt`;
   document.body.appendChild(link);
   link.click();
